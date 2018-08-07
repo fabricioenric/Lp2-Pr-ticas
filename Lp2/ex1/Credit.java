6c221c6b851469c0c978ff565b896edf069f2222
Fabricio Enric Smith Freire Silva <37123545+fabricioenric@users.noreply.github.com>
2018-08-07 08:33:39 -0300

    Commit do primeiro exercicios, dos créditos

---
+++ Lp2/ex1/Credit.java
@@ -0,0 +1,50 @@
+public class Credit{
+  	private int creditos;
+	
+	public Credit(){
+		this.creditos = 1000;
+	}
+
+	public Credit(int creditos){
+		this.creditos = creditos;
+	}
+
+	public int getCreditos(){
+		return this.creditos;
+	}
+	
+	public void setCreditos(int creditos){
+		this.creditos = creditos;
+	}
+
+	public void topUp(int creditos){
+		this.creditos+=creditos;
+		System.out.println("Foram adicionados "+ creditos +" creditos.");
+	}
+
+	public void consumir(int creditos){
+		int total = this.creditos-creditos;
+		if(creditos > this.creditos){
+			System.out.println("Não possui creditos suficientes para retirar.");
+			this.creditos = 0;
+		}
+			
+		else{
+			System.out.println("Creditos retirados com sucesso.\nForam retirados: "+ creditos +" creditos.");
+		}
+	} 
+
+	public static void main(String[] args){
+		Credit s1 = new Credit(100);
+		Credit s2 = new Credit(200);	
+		Credit s3 = new Credit(300);
+		Credit s4 = new Credit(400);
+		s1.topUp(100);
+		s2.consumir(300);
+		System.out.println("Saldo de credito de s1: "+s1.getCreditos());
+		System.out.println("Saldo de credito de s2: "+s2.getCreditos());
+		System.out.println("Saldo de credito de s3: "+s3.getCreditos());
+		System.out.println("Saldo de credito de s4: "+s4.getCreditos());
+	}
+
+}
